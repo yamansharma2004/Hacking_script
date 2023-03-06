cd993fcf43fd9c3b9a541bfcac1966b0c318bb9f
yaman <67410636+yamansharma2004@users.noreply.github.com>
2023-03-06 21:50:04 +0530

    Create dns_spoof.py
    For windows use pip install --pre scapy[complete] and pip install subprocess

---
+++ Networking/Scapy/CLI/DNS_Spoof/dns_spoof.py
@@ -0,0 +1,52 @@
+import scapy.all as scapy 
+from threading import Thread
+from time import sleep
+from os import system
+import  subprocess
+##################################
+#DNS flush
+system("ipcnfig /flushdns")
+system("cls")
+###########################################
+#Emty list 
+l=[]
+dic={}
+###############################
+#count the list 
+def count_list(list_):
+    for i in list_:
+        count=0
+        for j in range(len(list_)):
+            if i==list_[j]:
+                count+=1
+                if count>1:
+                    dic[i]=count
+    print(dic)
+#################################
+#Printing in sniff 
+def print_(packet):
+    if packet.haslayer(scapy.DNS):
+        if packet.haslayer(scapy.DNSRR):
+            name=packet[scapy.DNSRR].rrname
+            try:
+                name=name.decode("ascii")
+            except:
+                pass 
+            l.append(name)
+###################################
+#Printing
+def thread_printing():
+    while True:
+        print("Data:-")
+        count_list(l)
+        sleep(5)
+        subprocess.call("ipconfig /flushdns")
+        system("cls")
+t=Thread(target=thread_printing)
+t.daemon=True
+t.start()
+try:
+    scapy.sniff(prn=print_)
+except:
+    print("printing list")
+    pass
